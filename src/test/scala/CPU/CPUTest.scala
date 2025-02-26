eff881db5619c75b277aa33bfcc4aa62e523c017
xidianswq <3209507800@qq.com>
2025-02-26 16:04:08 +0800

    CPUTest change should text

--- src/test/scala/CPU/CPUTest.scala
+++ src/test/scala/CPU/CPUTest.scala
@@ -5,7 +5,7 @@ import org.scalatest._
 import chiseltest._
 
 class CPUTest extends FlatSpec with ChiselScalatestTester {
-  "mycpu" should "work through hex" in {
+  "mycpu" should "work well" in {
     test(new Top) { c =>
       while (!c.io.exit.peek().litToBoolean){
         c.clock.step(1)
