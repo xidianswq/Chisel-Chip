569c79682be706f74b8abd64e9f29527c8835859
xidianswq <3209507800@qq.com>
2025-03-06 17:17:03 +0800

    add CPU_pipeline directory

---
+++ src/main/scala/CPU_pipeline/Top.scala
@@ -0,0 +1,24 @@
+package cpu
+
+import chisel3._
+import chisel3.util._
+
+
+class Top extends Module{
+    val io = IO(new Bundle{
+        val globalpointer = Output(UInt(32.W))
+        val exit = Output(Bool())
+    })
+
+    val core = Module(new Core)
+    val instmem = Module(new Mem)
+
+    core.io.instmem <> instmem.io.instmem
+    core.io.datamem <> instmem.io.datamem
+    io.globalpointer := core.io.globalpointer
+    io.exit := core.io.exit
+}
+
+object TopOption extends App {
+    (new chisel3.stage.ChiselStage).emitVerilog(new Top(), Array("--target-dir", "generated"))
+}
---
+++ src/test/scala/CPU_pipeline/CPUTest.scala
@@ -0,0 +1,17 @@
+package cpu
+
+import chisel3._
+import org.scalatest._
+import chiseltest._
+
+class CPUTest extends FlatSpec with ChiselScalatestTester {
+  behavior of "mycpu" 
+  it should "work well through hex" in {
+    test(new Top) { dut =>
+      while (!dut.io.exit.peek().litToBoolean){
+        dut.clock.step(1)
+      }
+      //c.io.globalpointer.expect(1.U)
+    }
+  }
+}
