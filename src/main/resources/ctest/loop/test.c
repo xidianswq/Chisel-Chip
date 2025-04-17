aa51791c293b7b36f242337df61ec085a20f8fd8
231220000-Zhang San <zhangsan@foo.com>
2025-04-17 08:59:42 -0700

    1. updata loop c file

--- src/main/resources/ctest/loop/test.c
+++ src/main/resources/ctest/loop/test.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main()
 {
-        int i = 0;
-        int j = 0;
+        unsigned int i = 0;
+        unsigned int j = 0;
         for(i=0;i<10;i=i+1){
         	for(j=0;j<10;j=j+2){
 			if(j>=i)break;
