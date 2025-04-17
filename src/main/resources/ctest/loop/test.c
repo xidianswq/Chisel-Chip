689c3160a44fcb5f399447e213577995312f74e9
231220000-Zhang San <zhangsan@foo.com>
2025-04-17 08:49:28 -0700

    1. updata loop c file

--- src/main/resources/ctest/loop/test.c
+++ src/main/resources/ctest/loop/test.c
@@ -3,9 +3,9 @@ int main()
 {
         int i = 0;
         int j = 0;
-        for(i=0;i<10;i++){
+        for(i=0;i<10;i=i+1){
         	for(j=0;j<10;j=j+2){
-			if(j>i)break;
+			if(j>=i)break;
         	}
         }
         asm volatile("unimp");
