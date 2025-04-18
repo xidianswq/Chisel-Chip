fd37e1d946574e5e961d5ff9c59fca1fe2d839c2
231220000-Zhang San <zhangsan@foo.com>
2025-04-17 21:24:50 -0700

    1. updata loop c file

--- src/main/resources/ctest/loop/test.c
+++ src/main/resources/ctest/loop/test.c
@@ -2,9 +2,9 @@
 int main()
 {
         unsigned int i = 0;
-        unsigned int j = 5;
+        unsigned int j = 7;
         unsigned int k = 0;
-        while(i<10){
+        while(i<15){
 		if(j>=i)k=k+1;
 		i=i+1;
         }
