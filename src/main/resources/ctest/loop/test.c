05961a914337c048f92318fed99842511982cb7b
231220000-Zhang San <zhangsan@foo.com>
2025-04-20 03:46:19 -0700

    1. updata loop ctest

--- src/main/resources/ctest/loop/test.c
+++ src/main/resources/ctest/loop/test.c
@@ -4,8 +4,8 @@ int main()
         unsigned int i = 0;
         unsigned int j = 100;
         unsigned int k = 0;
-	for(i<0;i<100;i++){
-		for(j=100;j>0;j--){
+	for(i<0;i<50;i++){
+		for(j=100-i;j>50;j--){
 			if(j>i)k++;
 			else k--;
 		}
